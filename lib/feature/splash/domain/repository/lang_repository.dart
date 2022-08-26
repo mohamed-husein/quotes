3e26fe4a40be18731146d6cf450a733979056806
mohamed-husein <102866604+mohamed-husein@users.noreply.github.com>
2022-08-26 16:26:16 +0200

    Delete lib/feature/splash/domain directory

--- lib/feature/splash/domain/repository/lang_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:quotes/core/error/failure.dart';
-
-abstract class BaseLangRepository
-{
-  Future<Either<Failure,bool>> changeLang({required String langCode});
-  Future<Either<Failure,String>> getSaved();
-}
